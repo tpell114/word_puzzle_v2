918847ce7a37c59695cae664fb01a08d07132cc2
calo247 <tyler.p@shaw.ca>
2025-02-21 15:59:36 -0700

    added interfaces

---
+++ src/WordRepositoryServer.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+
+public interface WordRepositoryServer extends Remote {
+    public boolean createWord(String word) throws RemoteException;
+    public boolean removeWord(String word) throws RemoteException;
+    public boolean checkWord(String word) throws RemoteException;
+    public String getRandomWord(int length) throws RemoteException;
+}
