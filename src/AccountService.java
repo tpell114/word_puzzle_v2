7ac1690132421febcf1a928bcd42ea91b32241e9
Juan Diego Serrato <jserr894@mtroyal.ca>
2025-02-22 00:15:30 -0700

    added account microservice

---
+++ src/AccountService.java
@@ -0,0 +1,5 @@
+import java.rmi.server.UnicastRemoteObject;
+
+public class AccountService extends UnicastRemoteObject implements AccountServiceInterface {
+    
+}
---
+++ src/AccountServiceInterface.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+
+public interface AccountServiceInterface extends Remote {
+    boolean registerUser(String username) throws RemoteException;
+    int getUserScore(String username) throws RemoteException;
+    void updateUserScore(String username, boolean won) throws RemoteException;
+    String getUserStats(String username) throws RemoteException;
+}
